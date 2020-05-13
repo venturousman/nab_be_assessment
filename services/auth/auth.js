18446f5ea904afd6747ad8900a762dab5ef2d742
tinhoangnguyen <tinhoang.nguyen@hotschedules.com>
2020-05-13 20:51:54 +0700

    demonstrate different styles of inter-service communication

--- services/auth/auth.js
+++ services/auth/auth.js
@@ -1,7 +1,26 @@
 
+// demonstrate the communication with Auth service by calling API
+// const https = require('https');  // can use built-in library of Node: https
+const axios = require('axios');  // or use 3rd party library: axios
+
 var auth = {
     login: function (req, res, next) {
         // TBD
+        // call the auth service API to authenticate and get back token
+        // https.post('https://authentication-service')
+        const info = {
+            username: 'user1@email.com',
+            password: 'some_hashed_password'
+        };
+
+        axios.post('https://authentication-service/login', info)
+            .then((res) => {
+                console.log(`statusCode: ${res.statusCode}`)
+                console.log(res)
+            })
+            .catch((error) => {
+                console.error(error)
+            });
     },
     logout: function (req, res, next) {
         // TBD
--- services/order/order.js
+++ services/order/order.js
@@ -1,3 +1,12 @@
+// demonstrate the communication with Order service by using message queue (Amazon SQS)
+// Load the AWS SDK for Node.js
+const AWS = require('aws-sdk');
+
+// Set the region 
+AWS.config.update({ region: 'us-east-1' });
+
+// Create an SQS service object
+const sqs = new AWS.SQS({ apiVersion: '2020-05-13' });
 
 var order = {
     get: function (req, res, next) {
@@ -8,6 +17,37 @@ var order = {
     },
     create: function (req, res, next) {
         // TBD
+        // Send message to Amazon SQS to send information about order
+        const { userEmail, itemName, itemPrice, quantity } = req.body;
+        let orderData = { userEmail, itemName, itemPrice, quantity };
+        var params = {
+            MessageAttributes: {
+                'userEmail': {
+                    DataType: 'String',
+                    StringValue: userEmail
+                },
+                'itemName': {
+                    DataType: 'String',
+                    StringValue: itemName
+                },
+                'itemPrice': {
+                    DataType: 'Number',
+                    StringValue: itemPrice
+                },
+                'quantity': {
+                    DataType: 'Number',
+                    StringValue: quantity
+                }
+            },
+            MessageBody: JSON.stringify(orderData),
+            QueueUrl: 'SQS_QUEUE_URL'   // TBD
+        };
+        let promise = sqs.sendMessage(params).promise();
+        promise.then((data) => {
+            console.log('Success', data.MessageId);
+        }).catch((err) => {
+            console.log('Error', err);
+        });
     },
     update: function (req, res, next) {
         // TBD
